b636e1a7c26ef4e1b37f9f11a489bc7f3aa5c9d2
KouzaiHiroki <kouzaimelnoa@outlook.jp>
2026-03-29 15:26:05 +0900

    implement TodoItem class #3

---
+++ 2_TodoList/TodoItem.java
@@ -0,0 +1,34 @@
+public class TodoItem {
+    private int id;
+    private String title;
+    private boolean done;
+
+    public TododItem(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.done = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    @Override
+    public String toString() {
+        return (done ? "[完了]" : "未完了[") + id + ": " + title;
+    }
+        
+
+}
