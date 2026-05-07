cdd437700499e2ec28efa4b8981ab48e180f355f
KouzaiHiroki <kouzaimelnoa@outlook.jp>
2026-05-07 22:58:47 +0900

    implement js of app.js

---
+++ 5_WebToDo/app.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const list = document.getElementById("todo-list");
+
+addBtn.addEventListener("click", () => {
+    const text = input.value.trim();
+    if (text === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    list.appendChild(li);
+    input.value = "";
+});
